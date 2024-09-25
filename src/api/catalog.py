859a3bee39f5cd5b913075271ef6e6d90d342339
David Voronov <davidvoronof@gmail.com>
2024-09-25 14:00:16 -0700

    testing catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import sqlalchemy
+from src import database as db
 
 router = APIRouter()
 
@@ -9,12 +11,25 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
 
+    output = ""
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+        for row in result:
+            output = f"{output}\n{row}"
+
     return [
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
-        ]
+        {
+            "row": output,
+
+        }
+    ]
+
+    # return [
+    #         {
+    #             "sku": "RED_POTION_0",
+    #             "name": "red potion",
+    #             "quantity": 1,
+    #             "price": 100,
+    #             "potion_type": [100, 0, 0, 0],
+    #         }
+    #     ]
