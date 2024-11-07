2ea3853e5cb066d443b6f78d07481215821945d6
David Voronov <davidvoronof@gmail.com>
2024-11-06 17:08:42 -0800

    update: ledgerized catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -3,6 +3,10 @@ import sqlalchemy
 from src.utils import database as db
 from src.utils import log
 from src.utils import jsonify
+import re
+import json
+from src import prices
+
 
 router = APIRouter()
 
@@ -15,15 +19,57 @@ def get_catalog():
     # LOGGING
     log.post_log("/catalog")
 
-    with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM catalog WHERE quantity > 0"))
-    row_list = result.fetchall()
-    if (row_list == []):
+    # Select my top 6 largest quantity potions
+    retrieve_catalog_query = sqlalchemy.text('''
+        SELECT
+            sku,
+            sku AS name,
+            CAST(SUM(quantity) AS INTEGER) AS quantity
+        FROM poshin_ledger
+        GROUP BY sku
+        HAVING SUM(quantity) > 0
+        LIMIT 6
+    ''')
+    try:
+        with db.engine.begin() as connection:
+            catalog_result = connection.execute(retrieve_catalog_query)
+    except Exception as e:
+        print(e)
         return []
 
-    # Currently just selects the top 6 rows.
-    # Future: implement better logic for selecting what potions need to be sold
-    if len(row_list) < 6:
-        return jsonify.rows_to_json(row_list)
-    else:
-        return jsonify.rows_to_json(row_list[0:6])
+    rows = []
+    for row in catalog_result.mappings():
+        row = dict(row)
+        row['price'] = get_price(row['sku'])
+        row['potion_type'] = get_type(row['sku'])
+        rows.append(row)
+
+    return json.loads(json.dumps(rows, default=str))
+
+def get_price(sku: str):
+    price = 0
+    for ml_quantity, color in re.findall(r"(\d+)([a-z]+)", sku):
+        ml_quantity = int(ml_quantity) # typecasting to int
+        if color == "red":
+            price += ml_quantity * prices.RED_PRICE_PER_ML
+        elif color == "green":
+            price += ml_quantity * prices.GREEN_PRICE_PER_ML
+        elif color == "blue":
+            price += ml_quantity * prices.BLUE_PRICE_PER_ML
+        elif color == "dark":
+            price += ml_quantity * prices.DARK_PRICE_PER_ML
+    return price
+
+def get_type(sku: str):
+    type = [0,0,0,0]
+    for ml_quantity, color in re.findall(r"(\d+)([a-z]+)", sku):
+        ml_quantity = int(ml_quantity) # typecasting to int
+        if color == "red":
+            type[0] = ml_quantity
+        elif color == "green":
+            type[1] = ml_quantity
+        elif color == "blue":
+            type[2] = ml_quantity
+        elif color == "dark":
+            type[3] = ml_quantity
+    return type
